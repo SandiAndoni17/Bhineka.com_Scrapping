e4872a614fb93479f4100561f879c6f0dc9dff42
sandiandoni17 <sandiandoni17@gmail.com>
2024-03-19 12:26:45 +0700

    oke

---
+++ main.py
@@ -0,0 +1,10 @@
+import requests
+from bs4 import BeautifulSoup
+ # Making a GET request
+r = requests.get('https://www.bhinneka.com/jual?cari=iphone')
+# check status code for response received
+# success code - 200
+print(r)
+# Parsing the HTML
+soup = BeautifulSoup(r.content, 'html.parser')
+print(soup.prettify())
